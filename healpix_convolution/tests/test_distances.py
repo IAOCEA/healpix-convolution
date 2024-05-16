f25e8769d120947930772755c850e92f165ba206
Justus Magin <keewis@posteo.de>
2024-05-16 15:43:54 +0200

    test the distances

---
+++ healpix_convolution/tests/test_distances.py
@@ -0,0 +1,17 @@
+import numpy as np
+import pytest
+
+import healpix_convolution.distances as hds
+
+
+@pytest.mark.parametrize(
+    ["neighbours", "expected"],
+    (
+        (np.array([[1, 0, 2, 3]]), np.array([[0, 0.25637566, 0.3699723, 0.25574741]])),
+        (np.array([[2, 71, 8]]), np.array([[0, 0.25637566, 0.25574741]])),
+    ),
+)
+def test_distances_numpy(neighbours, expected):
+    actual = hds.distances(neighbours, resolution=2, indexing_scheme="nested")
+
+    np.testing.assert_allclose(actual, expected)
