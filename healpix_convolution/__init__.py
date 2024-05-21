83e3fd42308502fd7cf1826a6fb8aa8a358a389d
Justus Magin <keewis@posteo.de>
2024-05-21 16:12:00 +0200

    expose the main functionality at the top level

--- healpix_convolution/__init__.py
+++ healpix_convolution/__init__.py
@@ -1,5 +1,9 @@
 from importlib.metadata import version
 
+from healpix_convolution import kernels  # noqa: F401
+from healpix_convolution.distances import distances  # noqa: F401
+from healpix_convolution.neighbours import neighbours  # noqa: F401
+
 try:
     __version__ = version("healpix_convolution")
 except Exception:
