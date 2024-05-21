a6e22d58508746712188dc765e16aebd0bdcf08a
Justus Magin <keewis@posteo.de>
2024-05-21 16:15:06 +0200

    directly import the used functions

--- healpix_convolution/kernels/gaussian.py
+++ healpix_convolution/kernels/gaussian.py
@@ -1,10 +1,9 @@
 import healpy as hp
 import numpy as np
 
-import healpix_convolution.distances as dist
-import healpix_convolution.neighbours as nb
-
-from .common import create_sparse
+from healpix_convolution.distances import distances
+from healpix_convolution.kernels.common import create_sparse
+from healpix_convolution.neighbours import neighbours
 
 
 def gaussian_kernel(
@@ -18,17 +17,16 @@ def gaussian_kernel(
     cell_distance = hp.nside2resol(2**resolution, arcmin=False)
     ring = int((truncate * sigma / cell_distance) // 2)
 
-    neighbours = nb.neighbours(
+    nb = neighbours(
         cell_ids, resolution=resolution, indexing_scheme=indexing_scheme, ring=ring
     )
-    distances = dist.distances(
-        neighbours, resolution=resolution, indexing_scheme=indexing_scheme
-    )
+    d = distances(nb, resolution=resolution, indexing_scheme=indexing_scheme)
 
     sigma2 = sigma * sigma
-    phi_x = np.exp(-0.5 / sigma2 * distances**2)
+    phi_x = np.exp(-0.5 / sigma2 * d**2)
 
-    kernel = create_sparse(cell_ids, neighbours, phi_x)
+    shape = (12 * 4**resolution, 12 * 4**resolution)
+    kernel = create_sparse(cell_ids, nb, phi_x, shape=shape)
 
     # normalize
     return kernel / np.sum(kernel, axis=1)
