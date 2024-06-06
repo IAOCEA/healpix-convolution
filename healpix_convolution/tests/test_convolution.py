ae0f5a22e0325746a8963ed645f21bd3aabc2f2d
Justus Magin <keewis@posteo.de>
2024-06-06 15:54:56 +0200

    refactor the test to be simpler

--- healpix_convolution/tests/test_convolution.py
+++ healpix_convolution/tests/test_convolution.py
@@ -1,22 +1,17 @@
+import hypothesis
 import hypothesis.extra.numpy as npst
 import hypothesis.strategies as st
 import numpy as np
+import pytest
 import sparse
-from hypothesis import given
+from hypothesis import given, settings
 
 from healpix_convolution import convolution
 
 
-@given(
-    npst.arrays(
-        shape=st.sampled_from([(5,), (10, 5)]),
-        dtype=st.sampled_from(
-            ["float64", "float32", "int8", "int16", "int32", "int64"]
-        ),
-    )
-)
-def test_numpy_convolve(data):
-    kernel = sparse.COO.from_numpy(
+@pytest.fixture
+def rolling_mean_kernel():
+    kernel = (
         np.array(
             [
                 [1, 1, 0, 0, 1],
@@ -29,6 +24,22 @@ def test_numpy_convolve(data):
         / 3
     )
 
+    return sparse.COO.from_numpy(kernel, fill_value=0)
+
+
+@given(
+    data=npst.arrays(
+        shape=st.sampled_from([(5,), (10, 5)]),
+        # TODO: figure out how to deal with floating point values
+        dtype=st.sampled_from(["int16", "int32", "int64"]),
+    ),
+)
+@settings(
+    deadline=1000,
+    suppress_health_check=[hypothesis.HealthCheck.function_scoped_fixture],
+)
+def test_numpy_convolve(data, rolling_mean_kernel):
+    kernel = rolling_mean_kernel
     actual = convolution.convolve(data, kernel)
 
     padding = [(0, 0)] * (data.ndim - 1) + [(1, 1)]
