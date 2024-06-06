1c33d660ff0bf1af8b9380ff00b410ab165a6a4d
Justus Magin <keewis@posteo.de>
2024-06-06 11:28:54 +0200

    thin wrapper around `opt_einsum.contract`

---
+++ healpix_convolution/convolution.py
@@ -0,0 +1,20 @@
+import dask.array as da
+import opt_einsum
+
+
+def convolve(arr, kernel, **kwargs):
+    """convolve an array using a pre-computed sparse kernel matrix
+
+    Parameters
+    ----------
+    arr : array-like
+        The data to convolve. The dimension to contract must be the last axis.
+    kernel : array-like
+        2-dimensional sparse matrix. Columns is the original pixels, rows the output pixels.
+    axis : int, optional
+        The axis along which to apply the convolution
+    """
+    if isinstance(arr, da.Array) and not isinstance(kernel, da.Array):
+        kernel = da.from_array(kernel)
+
+    return opt_einsum.contract(arr, kernel, "...a,ba->...b", **kwargs)
