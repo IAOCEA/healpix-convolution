8e413c15612ce00e1fe77c080635af53058c29f2
Justus Magin <keewis@posteo.de>
2024-07-25 10:59:56 +0200

    attach the ring to the kernel's metadata

--- healpix_convolution/xarray/kernels/gaussian.py
+++ healpix_convolution/xarray/kernels/gaussian.py
@@ -4,6 +4,16 @@ import xdggs  # noqa: F401
 from healpix_convolution.kernels import gaussian
 
 
+def compute_ring(grid_info, sigma, kernel_size, truncate):
+    if kernel_size is not None:
+        return int(kernel_size // 2)
+    else:
+        import healpy as hp
+
+        cell_distance = hp.nside2resol(2**grid_info.resolution, arcmin=False)
+        return int((truncate * sigma / cell_distance) // 2)
+
+
 def gaussian_kernel(
     cell_ids, sigma: float, truncate: float = 4.0, kernel_size: int | None = None
 ):
@@ -25,12 +35,12 @@ def gaussian_kernel(
     kernel : xarray.DataArray
         The kernel as a sparse matrix.
     """
-    grid = cell_ids.dggs.grid_info
+    grid_info = cell_ids.dggs.grid_info
 
     padded_cell_ids, matrix = gaussian.gaussian_kernel(
         cell_ids.data,
-        resolution=grid.resolution,
-        indexing_scheme=grid.indexing_scheme,
+        resolution=grid_info.resolution,
+        indexing_scheme=grid_info.indexing_scheme,
         sigma=sigma,
         truncate=truncate,
         kernel_size=kernel_size,
@@ -45,6 +55,9 @@ def gaussian_kernel(
         "kernel_type": "gaussian",
         "method": "continuous",
         "sigma": sigma,
+        "ring": compute_ring(
+            kernel_size=kernel_size, grid_info=grid_info, truncate=truncate, sigma=sigma
+        ),
     } | size_param
 
     return xr.DataArray(
