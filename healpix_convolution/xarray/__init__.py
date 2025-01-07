c7c2adc207825e288289518a492e0271e275acf8
Justus Magin <keewis@posteo.de>
2025-01-07 12:07:50 +0100

    add `__all__` for `healpix_convolution.xarray`

--- healpix_convolution/xarray/__init__.py
+++ healpix_convolution/xarray/__init__.py
@@ -1,3 +1,5 @@
-from healpix_convolution.xarray import kernels  # noqa: F401
-from healpix_convolution.xarray.convolution import convolve  # noqa: F401
-from healpix_convolution.xarray.padding import pad  # noqa: F401
+from healpix_convolution.xarray import kernels
+from healpix_convolution.xarray.convolution import convolve
+from healpix_convolution.xarray.padding import pad
+
+__all__ = ["kernels", "convolve", "pad"]
