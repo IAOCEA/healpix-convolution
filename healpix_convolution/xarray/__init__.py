418a21901292d9c978b05893a21b9ac6141ebbe6
Justus Magin <keewis@posteo.de>
2025-01-07 12:18:03 +0100

    document the `xarray` API for `healpix-convolution`

--- healpix_convolution/xarray/__init__.py
+++ healpix_convolution/xarray/__init__.py
@@ -1,5 +1,5 @@
 from healpix_convolution.xarray import kernels
 from healpix_convolution.xarray.convolution import convolve
-from healpix_convolution.xarray.padding import pad
+from healpix_convolution.xarray.padding import Padding, pad  # noqa: F401
 
 __all__ = ["kernels", "convolve", "pad"]
