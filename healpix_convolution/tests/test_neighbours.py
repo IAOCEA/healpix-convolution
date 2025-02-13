a1da95ede1abca04fad0a80d05f638ad4ab2c09d
Justus Magin <keewis@posteo.de>
2025-02-13 15:36:25 +0100

    adapt the neighbours search tests

--- healpix_convolution/tests/test_neighbours.py
+++ healpix_convolution/tests/test_neighbours.py
@@ -1,11 +1,11 @@
-import healpy as hp
+import cdshealpix
 import hypothesis.strategies as st
 import numpy as np
 import pytest
 from hypothesis import given
 from xdggs import HealpixInfo
 
-from healpix_convolution.neighbours import generate_offsets, neighbours
+from healpix_convolution.neighbours import neighbours
 
 try:
     import dask.array as da
@@ -18,19 +18,14 @@ except ImportError:  # pragma: no cover
 requires_dask = pytest.mark.skipif(not has_dask, reason="needs dask.array")
 
 
-@given(st.integers(min_value=0, max_value=200))
-def test_generate_offsets(ring):
-    kernel_size = 2 * ring + 1
-    kernel = np.zeros(shape=(kernel_size, kernel_size))
-
-    for x, y in generate_offsets(ring):
-        kernel[x + ring, y + ring] = 1
-
-    assert np.sum(kernel) == kernel_size**2
-
-
 @pytest.mark.parametrize("resolution", [1, 2, 4, 6])
-@pytest.mark.parametrize("indexing_scheme", ["ring", "nested"])
+@pytest.mark.parametrize(
+    "indexing_scheme",
+    [
+        pytest.param("ring", marks=pytest.mark.skip(reason="not implemented yet")),
+        "nested",
+    ],
+)
 @pytest.mark.parametrize("dask", [pytest.param(True, marks=requires_dask), False])
 def test_neighbours_ring1_manual(resolution, indexing_scheme, dask):
     if dask:
@@ -38,29 +33,30 @@ def test_neighbours_ring1_manual(resolution, indexing_scheme, dask):
     else:
         xp = np
 
+    # no other scheme supported
+    reference_neighbours = cdshealpix.nested.neighbours
+
     grid_info = HealpixInfo(level=resolution, indexing_scheme=indexing_scheme)
 
     cell_ids = xp.arange(12 * 4**resolution)
 
     actual = neighbours(cell_ids, grid_info=grid_info, ring=1)
 
-    nside = grid_info.nside
-    nest = grid_info.nest
-    expected = hp.get_all_neighbours(nside, np.asarray(cell_ids), nest=nest).T
+    expected = reference_neighbours(np.asarray(cell_ids), depth=grid_info.level)
 
-    actual_ = np.asarray(actual[:, 1:])
-
-    np.testing.assert_equal(actual_, expected)
+    np.testing.assert_equal(np.sort(actual, axis=1), np.sort(expected, axis=1))
 
 
-@given(st.integers(min_value=1, max_value=7), st.sampled_from(["ring", "nested"]))
+@given(st.integers(min_value=1, max_value=7), st.sampled_from(["nested"]))
 def test_neighbours_ring1(resolution, indexing_scheme):
+    # no other scheme supported
+    reference_neighbours = cdshealpix.nested.neighbours
+
     cell_ids = np.arange(12 * 4**resolution)
 
     grid_info = HealpixInfo(level=resolution, indexing_scheme=indexing_scheme)
     actual = neighbours(cell_ids, grid_info=grid_info, ring=1)
-    nside = grid_info.nside
-    nest = grid_info.nest
-    expected = hp.get_all_neighbours(nside, cell_ids, nest=nest).T
 
-    np.testing.assert_equal(actual[:, 1:], expected)
+    expected = reference_neighbours(cell_ids, depth=grid_info.level)
+
+    np.testing.assert_equal(np.sort(actual, axis=1), np.sort(expected, axis=1))
