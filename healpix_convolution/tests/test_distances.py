9315a12d56859d0fb694e305550db02359a0e92e
Justus Magin <keewis@posteo.de>
2025-02-13 16:48:04 +0100

    make sure multi-dimensional cell ids still work

--- healpix_convolution/tests/test_distances.py
+++ healpix_convolution/tests/test_distances.py
@@ -20,6 +20,7 @@ def test_angle_between_vectors():
     (
         (np.array([[1, 0, 2, 3]]), np.array([[0, 0.25637566, 0.3699723, 0.25574741]])),
         (np.array([[2, 71, 8]]), np.array([[0, 0.25637566, 0.25574741]])),
+        (np.array([[2, 71], [2, 8]]), np.array([[0, 0.25637566], [0, 0.25574741]])),
     ),
 )
 def test_angular_distances_numpy(neighbours, expected):
